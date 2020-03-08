7beb99b86348a2d63d934642af2a4df23b30d792
OTB-X <58358801+OTB-X@users.noreply.github.com>
2020-03-08 16:58:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
-from IPython.display import Image
 
 
 
